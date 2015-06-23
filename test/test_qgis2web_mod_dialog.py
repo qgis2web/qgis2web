950a156803411aa57e379fd828f3765b7f13c0e4
Tom Chadwin <tom.chadwin@nnpa.org.uk>
2015-06-23 20:22:28 +0100

    Test maindialog import fix

--- test/test_qgis2web_mod_dialog.py
+++ test/test_qgis2web_mod_dialog.py
@@ -16,7 +16,7 @@ import unittest
 
 from PyQt4.QtGui import QDialogButtonBox, QDialog
 
-from main_dialog import MainDialog
+from maindialog import MainDialog
 
 from utilities import get_qgis_app
 QGIS_APP = get_qgis_app()
