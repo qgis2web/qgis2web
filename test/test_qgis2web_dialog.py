6877d81d1aed36b3e41846fb0aa67863416da0e7
Tom Chadwin <tom.chadwin@nnpa.org.uk>
2015-07-09 10:50:38 +0100

    Mouse clicks in tests, update tests for UI changes
    #80

--- test/test_qgis2web_dialog.py
+++ test/test_qgis2web_dialog.py
@@ -36,25 +36,37 @@ class qgis2web_classDialogTest(unittest.TestCase):
         """Runs after each test."""
         self.dialog = None
 
-    def test_preview_OL3(self):
-        """Preview OL3 (test_qgis2web_dialog.test_preview_OL3)."""
-        self.dialog.previewOL3()
-        # result = self.dialog.result()
-        # self.assertEqual(result, QDialog.Accepted)
+    def test_preview_default(self):
+        """Preview default (OL3) (test_qgis2web_dialog.test_preview_default)."""
+        self.dialog.previewMap()
+
+    def test_save_default(self):
+        """Save default (OL3) (test_qgis2web_dialog.test_save_default)."""
+        self.dialog.saveMap()
+
+    def test_toggle_Leaflet(self):
+        """Toggle to Leaflet (test_qgis2web_dialog.test_toggle_Leaflet)."""
+        QTest.mouseClick(self.dialog.leaflet, Qt.LeftButton)
 
     def test_preview_Leaflet(self):
         """Preview Leaflet (test_qgis2web_dialog.test_preview_Leaflet)."""
-        self.dialog.previewLeaflet()
-        # result = self.dialog.result()
-        # self.assertEqual(result, QDialog.Rejected)
-
-    def test_export_OL3(self):
-        """Export OL3 (test_qgis2web_dialog.test_export_OL3)."""
-        self.dialog.saveOL()
+        self.dialog.previewMap()
 
     def test_export_Leaflet(self):
         """Export Leaflet (test_qgis2web_dialog.test_export_Leaflet)."""
-        self.dialog.saveLeaf()
+        self.dialog.saveMap()
+
+    def test_toggle_OL3(self):
+        """Toggle to OL3 (test_qgis2web_dialog.test_toggle_OL3)."""
+        QTest.mouseClick(self.dialog.ol3, Qt.LeftButton)
+
+    def test_preview_OL3(self):
+        """Preview OL3 (test_qgis2web_dialog.test_preview_OL3)."""
+        self.dialog.previewMap()
+
+    def test_export_OL3(self):
+        """Export OL3 (test_qgis2web_dialog.test_export_OL3)."""
+        self.dialog.saveMap()
 
 if __name__ == "__main__":
     suite = unittest.makeSuite(qgis2web_classDialogTest)
