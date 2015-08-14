010146b059d4289e946910d2a822eba753974675
Tom Chadwin <tom.chadwin@nnpa.org.uk>
2015-08-14 17:14:01 +0100

    Handle empty label field
    #51

--- olwriter.py
+++ olwriter.py
@@ -240,7 +240,10 @@ def exportStyles(layers, folder):
         labelsEnabled = unicode(layer.customProperty("labeling/enabled")).lower() == "true"
         if (labelsEnabled):
             labelField = layer.customProperty("labeling/fieldName")
-            labelText = 'feature.get("%s")' % labelField.replace('"', '\\"')
+            if labelField != "":
+                labelText = 'feature.get("%s")' % labelField.replace('"', '\\"')
+            else:
+                labelText = '""'
         else:
             labelText = '""'
         defs = ""
