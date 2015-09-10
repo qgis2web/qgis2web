8e2e136b1e29df6f6607ac5ef6a86ae305190713
Tom Chadwin <tom.chadwin@nnpa.org.uk>
2015-09-10 16:55:40 +0100

    Cast count to str
    #105

--- leafletWriter.py
+++ leafletWriter.py
@@ -158,7 +158,7 @@ def writeLeaflet(iface, outputProjectFileName, width, height, full, layer_list, 
         new_field_names = []
         rawLayerName = i.name()
         safeLayerName = re.sub('[\W_]+', '', rawLayerName)
-        print count + ": " + safeLayerName
+        print unicode(count) + ": " + safeLayerName
         if i.type() == QgsMapLayer.VectorLayer:
             with open(outputIndex, 'a') as f5:
                 fields = i.pendingFields()
