5983c8093851bdb62d2b4ba64fdb04b0396ad4ba
Tom Chadwin <tom.chadwin@nnpa.org.uk>
2015-09-10 11:31:55 +0100

    Deliberate error
    #105

--- test/test_qgis2web_dialog.py
+++ test/test_qgis2web_dialog.py
@@ -77,7 +77,7 @@ class qgis2web_classDialogTest(unittest.TestCase):
     def test09_Leaflet_shp_pnt_simple(self):
         """Leaflet shape point simple (test_qgis2web_dialog.test_Leaflet_shp_pnt_simple)."""
         project = QgsProject.instance()
-        project.read(QFileInfo('../test_data/shp_point_simple.qgs'))
+        project = open('../test_data/shp_point_simpleX.qgs')
         self.dialog.leaflet.click()
         self.dialog.buttonExport.click()
 
