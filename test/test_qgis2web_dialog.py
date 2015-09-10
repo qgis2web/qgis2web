95187a4c39006aff76a3e8206b4571bc52f882e5
Tom Chadwin <tom.chadwin@nnpa.org.uk>
2015-09-10 11:28:06 +0100

    Relative path in test
    #105

--- test/test_qgis2web_dialog.py
+++ test/test_qgis2web_dialog.py
@@ -77,7 +77,7 @@ class qgis2web_classDialogTest(unittest.TestCase):
     def test09_Leaflet_shp_pnt_simple(self):
         """Leaflet shape point simple (test_qgis2web_dialog.test_Leaflet_shp_pnt_simple)."""
         project = QgsProject.instance()
-        project.read(QFileInfo('/home/travis/build/tomchadwin/qgis2web/test_data/shp_point_simpleX.qgs'))
+        project.read(QFileInfo('../test_data/shp_point_simple.qgs'))
         self.dialog.leaflet.click()
         self.dialog.buttonExport.click()
 
