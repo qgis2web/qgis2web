0317dfc67eb152aeb2be8f77c77bc6255f98dcf4
Tom Chadwin <tom.chadwin@nnpa.org.uk>
2015-11-27 09:40:24 +0000

    OL3 JSON point categorized style test

--- test/test_qgis2web_dialog.py
+++ test/test_qgis2web_dialog.py
@@ -447,7 +447,6 @@ class qgis2web_classDialogTest(unittest.TestCase):
         testOutput = testFile.read()
         testStyleFile = open(self.dialog.preview.url().toString().replace("file://","").replace("index.html", "styles/point_style.js"))
         testStyleOutput = testStyleFile.read()
-        print testStyleOutput
         testOutput += testStyleOutput
         self.assertEqual(testOutput, referenceOutput)
 
@@ -506,6 +505,9 @@ class qgis2web_classDialogTest(unittest.TestCase):
         self.dialog.ol3.click()
         testFile = open(self.dialog.preview.url().toString().replace("file://",""))
         testOutput = testFile.read()
+        testStyleFile = open(self.dialog.preview.url().toString().replace("file://","").replace("index.html", "styles/point_style.js")
+        testStyleOutput = testStyleFile.read()
+        testOutput += testStyleOutput
         self.assertEqual(testOutput, referenceOutput)
 
     def test31_OL3_line_categorized(self):
