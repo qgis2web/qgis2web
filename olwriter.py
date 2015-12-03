38448f9a7436067b675250dece762cbe76935629
Tom Chadwin <tom.chadwin@nnpa.org.uk>
2015-12-03 17:13:07 +0000

    PEP8

--- olwriter.py
+++ olwriter.py
@@ -262,8 +262,7 @@ function get%(n)sJson(geojson) {
     var features_%(n)s = format_%(n)s.readFeatures(geojson);
     jsonSource_%(n)s.addFeatures(features_%(n)s);
 }''' % {"name": layer.name(), "n": layerName,
-        "min": minResolution,
-        "max": maxResolution})
+        "min": minResolution, "max": maxResolution})
         else:
             return ('''var format_%(n)s = new ol.format.GeoJSON();
 var features_%(n)s = format_%(n)s.readFeatures(geojson_%(n)s);
--- utils.py
+++ utils.py
@@ -92,7 +92,7 @@ def exportLayers(layers, folder, precision, optimize, popupField, json):
         re.compile(r"([0-9]+\.[0-9]{%s})([0-9]+)" % unicode(int(precision))))
     for layer, encode2json, popup in zip(layers, json, popupField):
         if (layer.type() == layer.VectorLayer and
-            (layer.providerType() != "WFS" or encode2json)):
+                (layer.providerType() != "WFS" or encode2json)):
             layer = writeTmpLayer(layer, popup)
 
             tmpPath = os.path.join(layersFolder,
