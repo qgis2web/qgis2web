4ed2df54d017e33f3ce1e7ab7518ea24551c7fd4
Tom Chadwin <tom.chadwin@nnpa.org.uk>
2016-01-05 10:28:23 +0000

    First attempt at #148
    Not working, but I don't understand why - any ideas, anyone?

--- leafletWriter.py
+++ leafletWriter.py
@@ -121,7 +121,27 @@ def writeLeaflet(iface, outputProjectFileName, width, height, full, layer_list,
 
             elif i.type() == QgsMapLayer.RasterLayer:
                 if i.dataProvider().name() != "wms":
-                    in_raster = unicode(i.dataProvider().dataSourceUri())
+                    pipelayer = i
+                    pipeextent = pipelayer.extent()
+                    pipewidth, pipeheight = pipelayer.width(), pipelayer.height()
+                    piperenderer = pipelayer.renderer()
+                    pipeprovider = pipelayer.dataProvider()
+                    crs = pipelayer.crs().toWkt()
+                    pipe = QgsRasterPipe()
+                    pipe.set(pipeprovider.clone())
+                    pipe.set(piperenderer.clone())
+                    pipedFile = os.path.join(tempfile.gettempdir(),
+                                               safeLayerName +
+                                               '_pipe.tif')
+                    print pipedFile
+                    file_writer = QgsRasterFileWriter(pipedFile)
+                    file_writer.writeRaster(pipe,
+                                            pipewidth,
+                                            pipeheight,
+                                            pipeextent,
+                                            pipelayer.crs())
+
+                    in_raster = pipedFile
                     prov_raster = os.path.join(tempfile.gettempdir(),
                                                'json_' + safeLayerName +
                                                '_prov.tif')
@@ -136,7 +156,7 @@ def writeLeaflet(iface, outputProjectFileName, width, height, full, layer_list,
                                              unicode(extentRep.yMaximum())])
                     processing.runalg("gdalogr:warpreproject", in_raster,
                                       i.crs().authid(), "EPSG:4326", "", 0, 1,
-                                      0, -1, 75, 6, 1, False, 0, False, "",
+                                      5, 2, 75, 6, 1, False, 0, False, "",
                                       prov_raster)
                     processing.runalg("gdalogr:translate", prov_raster, 100,
                                       True, "", 0, "", extentRepNew, False, 0,
