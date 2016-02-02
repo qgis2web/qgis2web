eaaa3d23655818ce9bdbbc96d6bfc274a69c0233
Tom Chadwin <tom.chadwin@nnpa.org.uk>
2016-02-02 16:18:19 +0000

    Tidy OL3 JS output, format OL3 attribution

--- resources/qgis2web.js
+++ resources/qgis2web.js
@@ -1,177 +1,174 @@
 @MEASURECONTROL@
-      var container = document.getElementById('popup');
-      var content = document.getElementById('popup-content');
-      var closer = document.getElementById('popup-closer');
-      closer.onclick = function() {
-        container.style.display = 'none';
-        closer.blur();
-        return false;
-      };
-      var overlayPopup = new ol.Overlay({
-        element: container
-      });
+var container = document.getElementById('popup');
+var content = document.getElementById('popup-content');
+var closer = document.getElementById('popup-closer');
+closer.onclick = function() {
+    container.style.display = 'none';
+    closer.blur();
+    return false;
+};
+var overlayPopup = new ol.Overlay({
+    element: container
+});
 
-      var expandedAttribution = new ol.control.Attribution({
-        collapsible: false
-      });
+var expandedAttribution = new ol.control.Attribution({
+    collapsible: false
+});
 
-      var map = new ol.Map({
-        controls: ol.control.defaults({attribution:false}).extend([
-          @CONTROLS@
-        ]),
-        target: document.getElementById('map'),
-        renderer: 'canvas',
-        overlays: [overlayPopup],
-        layers: layersList,
-        view: new ol.View({
-          @VIEW@
-        })
-      });
-      map.getView().fit(@BOUNDS@, map.getSize());
+var map = new ol.Map({
+    controls: ol.control.defaults({attribution:false}).extend([
+        @CONTROLS@
+    ]),
+    target: document.getElementById('map'),
+    renderer: 'canvas',
+    overlays: [overlayPopup],
+    layers: layersList,
+    view: new ol.View({
+        @VIEW@
+    })
+});
+map.getView().fit(@BOUNDS@, map.getSize());
 
-      var NO_POPUP = 0
-      var ALL_FIELDS = 1
+var NO_POPUP = 0
+var ALL_FIELDS = 1
 
-      @POPUPLAYERS@
+@POPUPLAYERS@
 
-      var collection = new ol.Collection();
-      var featureOverlay = new ol.layer.Vector({
-        map: map,
-        source: new ol.source.Vector({
-          features: collection,
-          useSpatialIndex: false // optional, might improve performance
+var collection = new ol.Collection();
+var featureOverlay = new ol.layer.Vector({
+    map: map,
+    source: new ol.source.Vector({
+        features: collection,
+        useSpatialIndex: false // optional, might improve performance
+    }),
+    style: [new ol.style.Style({
+        stroke: new ol.style.Stroke({
+            color: '#f00',
+            width: 1
         }),
-        style: [new ol.style.Style({
-              stroke: new ol.style.Stroke({
-                color: '#f00',
-                width: 1
-              }),
-              fill: new ol.style.Fill({
-                color: 'rgba(255,0,0,0.1)'
-              }),
-              })],
-        updateWhileAnimating: true, // optional, for instant visual feedback
-        updateWhileInteracting: true // optional, for instant visual feedback
-      });
+        fill: new ol.style.Fill({
+            color: 'rgba(255,0,0,0.1)'
+        }),
+    })],
+    updateWhileAnimating: true, // optional, for instant visual feedback
+    updateWhileInteracting: true // optional, for instant visual feedback
+});
 
-      var doHighlight = @DOHIGHLIGHT@;
-      var doHover = @ONHOVER@;
+var doHighlight = @DOHIGHLIGHT@;
+var doHover = @ONHOVER@;
 
-      var highlight;
-      var onPointerMove = function(evt) {
-        if (!doHover && !doHighlight){
-          return;
-        }
-        var pixel = map.getEventPixel(evt.originalEvent);
-        var coord = evt.coordinate;
-        var popupField;
-        var popupText = '';
-        var currentFeature;
-        var currentLayer;
-        var currentFeatureKeys;
-        map.forEachFeatureAtPixel(pixel, function(feature, layer) {
-          currentFeature = feature;
-          currentLayer = layer;
-          currentFeatureKeys = currentFeature.getKeys();
-          var field = popupLayers[layersList.indexOf(layer) - 1];
-          if (field == NO_POPUP){          
-          }
-          else if (field == ALL_FIELDS){
+var highlight;
+var onPointerMove = function(evt) {
+    if (!doHover && !doHighlight) {
+        return;
+    }
+    var pixel = map.getEventPixel(evt.originalEvent);
+    var coord = evt.coordinate;
+    var popupField;
+    var popupText = '';
+    var currentFeature;
+    var currentLayer;
+    var currentFeatureKeys;
+    map.forEachFeatureAtPixel(pixel, function(feature, layer) {
+        currentFeature = feature;
+        currentLayer = layer;
+        currentFeatureKeys = currentFeature.getKeys();
+        var field = popupLayers[layersList.indexOf(layer) - 1];
+        if (field == NO_POPUP) {          
+        } else if (field == ALL_FIELDS) {
             popupText = '<table>';
-            for ( var i=0; i<currentFeatureKeys.length;i++) {
+            for (var i=0; i<currentFeatureKeys.length; i++) {
                 if (currentFeatureKeys[i] != 'geometry') {
-                    popupField = '<th>' + currentFeatureKeys[i] + ':</th><td>'+ currentFeature.get(currentFeatureKeys[i]) + '</td>';
+                    popupField = '<th>' + currentFeatureKeys[i] + ':</th>'
+                    popupField += '<td>'+ currentFeature.get(currentFeatureKeys[i]) + '</td>';
                     popupText = popupText + '<tr>' + popupField + '</tr>';
                 }
             }
             popupText = popupText + '</table>';
-          }
-          else{
+        } else {
             var value = feature.get(field);
-            if (value){
-              popupText = '<strong>' + field + ':</strong> ' + value;
+            if (value) {
+                popupText = '<strong>' + field + ':</strong> ' + value;
             }  
-          }@MEASURING@          
-        });
+        }
+        @MEASURING@          
+    });
 
-        if (doHighlight){
-          if (currentFeature !== highlight) {
+    if (doHighlight) {
+        if (currentFeature !== highlight) {
             if (highlight) {
-              featureOverlay.getSource().removeFeature(highlight);
+                featureOverlay.getSource().removeFeature(highlight);
             }
             if (currentFeature) {
-              var styleDefinition = currentLayer.getStyle().toString();
+                var styleDefinition = currentLayer.getStyle().toString();
 
-              if (currentFeature.getGeometry().getType() == 'Point') {
-                var radius = styleDefinition.split('radius')[1].split(' ')[1];
+                if (currentFeature.getGeometry().getType() == 'Point') {
+                    var radius = styleDefinition.split('radius')[1].split(' ')[1];
 
-                highlightStyle = new ol.style.Style({
-                image: new ol.style.Circle({
-                  fill: new ol.style.Fill({
-                    color: "@HIGHLIGHTFILL@"
-                  }),
-                  radius: radius
-                })
-               })
-              } else if (currentFeature.getGeometry().getType() == 'LineString') {
+                    highlightStyle = new ol.style.Style({
+                        image: new ol.style.Circle({
+                            fill: new ol.style.Fill({
+                                color: "@HIGHLIGHTFILL@"
+                            }),
+                            radius: radius
+                        })
+                    })
+                } else if (currentFeature.getGeometry().getType() == 'LineString') {
 
-                var featureWidth = styleDefinition.split('width')[1].split(' ')[1].replace('})','');
+                    var featureWidth = styleDefinition.split('width')[1].split(' ')[1].replace('})','');
 
-                highlightStyle = new ol.style.Style({
-                  stroke: new ol.style.Stroke(
-                    {
-                      color: '@HIGHLIGHTFILL@',
-                      lineDash: null,
-                      width: featureWidth
-                    }
-                  )
-                });
+                    highlightStyle = new ol.style.Style({
+                        stroke: new ol.style.Stroke({
+                            color: '@HIGHLIGHTFILL@',
+                            lineDash: null,
+                            width: featureWidth
+                        })
+                    });
 
-              } else {
-                highlightStyle = new ol.style.Style({
-                  fill: new ol.style.Fill({
-                    color: '@HIGHLIGHTFILL@'
-                  })
-                })
-              }
-              featureOverlay.getSource().addFeature(currentFeature);
-              featureOverlay.setStyle(highlightStyle);
+                } else {
+                    highlightStyle = new ol.style.Style({
+                        fill: new ol.style.Fill({
+                            color: '@HIGHLIGHTFILL@'
+                        })
+                    })
+                }
+                featureOverlay.getSource().addFeature(currentFeature);
+                featureOverlay.setStyle(highlightStyle);
             }
             highlight = currentFeature;
-          }
         }
+    }
 
-        if (doHover){
-          if (popupText) {
+    if (doHover) {
+        if (popupText) {
             overlayPopup.setPosition(coord);
             content.innerHTML = popupText;
             container.style.display = 'block';        
-          } else {
+        } else {
             container.style.display = 'none';
             closer.blur();
-          }
         }
-      };
+    }
+};
 
-      var onSingleClick = function(evt) {
-        if (doHover){
-          return;
-        }
-        var pixel = map.getEventPixel(evt.originalEvent);
-        var coord = evt.coordinate;
-        var popupField;
-        var popupText = '';
-        var currentFeature;
-        var currentFeatureKeys;
-        map.forEachFeatureAtPixel(pixel, function(feature, layer) {
-          currentFeature = feature;
-          currentFeatureKeys = currentFeature.getKeys();
-          var field = popupLayers[layersList.indexOf(layer) - 1];
-          if (field == NO_POPUP){          
-          }
-          else if (field == ALL_FIELDS){
+var onSingleClick = function(evt) {
+    if (doHover) {
+        return;
+    }
+    var pixel = map.getEventPixel(evt.originalEvent);
+    var coord = evt.coordinate;
+    var popupField;
+    var popupText = '';
+    var currentFeature;
+    var currentFeatureKeys;
+    map.forEachFeatureAtPixel(pixel, function(feature, layer) {
+        currentFeature = feature;
+        currentFeatureKeys = currentFeature.getKeys();
+        var field = popupLayers[layersList.indexOf(layer) - 1];
+        if (field == NO_POPUP) {          
+        } else if (field == ALL_FIELDS) {
             popupText = '<table>';
-            for ( var i=0; i<currentFeatureKeys.length;i++) {
+            for (var i=0; i<currentFeatureKeys.length; i++) {
                 if (currentFeatureKeys[i] != 'geometry') {
                     popupField = '<th>' + currentFeatureKeys[i] + ':</th><td>'
                     popupField += (currentFeature.get(currentFeatureKeys[i]) != null ? Autolinker.link(String(currentFeature.get(currentFeatureKeys[i]))) + '</td>' : '');
@@ -179,35 +176,37 @@
                 }
             }
             popupText = popupText + '</table>';
-          }
-          else{
-            var value = feature.get(field);
-            if (value){
-              popupText = '<strong>' + field + ':</strong> '+ value;
-            }  
-          }          
-        });
-
-        if (popupText) {
-            overlayPopup.setPosition(coord);
-            content.innerHTML = popupText;
-            container.style.display = 'block';        
         } else {
-          container.style.display = 'none';
-          closer.blur();
-        }
-      };
+            var value = feature.get(field);
+            if (value) {
+                popupText = '<strong>' + field + ':</strong> '+ value;
+            }  
+        }          
+    });
 
-      map.on('pointermove', function(evt) {
-        onPointerMove(evt);
-      });
-      map.on('singleclick', function(evt) {
-        onSingleClick(evt);
-      });@MEASURE@@GEOLOCATE@@GEOCODINGSCRIPT@
+    if (popupText) {
+        overlayPopup.setPosition(coord);
+        content.innerHTML = popupText;
+        container.style.display = 'block';        
+    } else {
+        container.style.display = 'none';
+        closer.blur();
+    }
+};
 
-      var attribution = document.getElementsByClassName('ol-attribution')[0];
-      var attributionList = attribution.getElementsByTagName('ul')[0];
-      var firstLayerAttribution = attributionList.getElementsByTagName('li')[0];
-      var qgis2webAttribution = document.createElement('li');
-      qgis2webAttribution.innerHTML = '<a href="https://github.com/tomchadwin/qgis2web">qgis2web</a>';
-      attributionList.insertBefore(qgis2webAttribution, firstLayerAttribution);
+map.on('pointermove', function(evt) {
+    onPointerMove(evt);
+});
+map.on('singleclick', function(evt) {
+    onSingleClick(evt);
+});
+@MEASURE@
+@GEOLOCATE@
+@GEOCODINGSCRIPT@
+
+var attribution = document.getElementsByClassName('ol-attribution')[0];
+var attributionList = attribution.getElementsByTagName('ul')[0];
+var firstLayerAttribution = attributionList.getElementsByTagName('li')[0];
+var qgis2webAttribution = document.createElement('li');
+qgis2webAttribution.innerHTML = '<a href="https://github.com/tomchadwin/qgis2web">qgis2web</a>';
+attributionList.insertBefore(qgis2webAttribution, firstLayerAttribution);
