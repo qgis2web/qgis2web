16c1666b0d5389f0d0699f14edcab18efcb2de0c
Tom Chadwin <tom.chadwin@nnpa.org.uk>
2016-04-19 16:11:06 +0100

    OL3 geolocate test

--- test/test_qgis2web_dialog.py
+++ test/test_qgis2web_dialog.py
@@ -1244,6 +1244,46 @@ class qgis2web_classDialogTest(unittest.TestCase):
         # Compare with control file
         self.assertEqual(test_output, control_output)
 
+    def test47_OL3_geolocate(self):
+        """OL3 geolocate user"""
+        layer_path = test_data_path('layer', 'airports.shp')
+        style_path = test_data_path('style', 'airports_single.qml')
+        layer = load_layer(layer_path)
+        layer.loadNamedStyle(style_path)
+
+        registry = QgsMapLayerRegistry.instance()
+        registry.addMapLayer(layer)
+
+        control_file = open(
+                test_data_path(
+                        'control', 'ol3_geolocate.js'), 'r')
+        control_output = control_file.read()
+
+
+        # Export to web map
+        self.dialog = MainDialog(IFACE)
+        self.dialog.paramsTreeOL.itemWidget(
+                self.dialog.paramsTreeOL.findItems(
+                        'Extent',
+                        (Qt.MatchExactly | Qt.MatchRecursive))[0],
+                1).setCurrentIndex(1)
+        # Check the 'Add scale bar' checkbox
+        self.dialog.items['Appearance'].get('Geolocate user').setCheckState(1, QtCore.Qt.Checked)
+        self.dialog.leaflet.click()
+
+        control_file = open(
+                test_data_path(
+                        'control', 'ol3_geolocate.js'), 'r')
+        control_output = control_file.read()
+
+
+        # Open the test file
+        test_output = read_output(self.dialog.preview.url().toString(), 'resources/qgis2web.js')
+
+        # Compare with control file
+        self.assertEqual(test_output, control_output)
+
+
 def read_output(url, path):
     """ Given a url for the index.html file of a preview or export and the
     relative path to an output file open the file and return it's contents as a
