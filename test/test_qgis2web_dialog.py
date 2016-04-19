5eb521101cd37fa7000576a5f6d52ab847eb32bd
Tom Chadwin <tom.chadwin@nnpa.org.uk>
2016-04-19 15:53:41 +0100

    OL3 address test

--- test/test_qgis2web_dialog.py
+++ test/test_qgis2web_dialog.py
@@ -1165,6 +1165,50 @@ class qgis2web_classDialogTest(unittest.TestCase):
         # Compare with control file
         self.assertEqual(test_output, control_output)
 
+    def test43_OL3_address(self):
+        """OL3 address search"""
+        layer_path = test_data_path('layer', 'airports.shp')
+        style_path = test_data_path('style', 'airports_single.qml')
+        layer = load_layer(layer_path)
+        layer.loadNamedStyle(style_path)
+
+        registry = QgsMapLayerRegistry.instance()
+        registry.addMapLayer(layer)
+
+        # Export to web map
+        self.dialog = MainDialog(IFACE)
+        self.dialog.paramsTreeOL.itemWidget(
+                self.dialog.paramsTreeOL.findItems(
+                        'Extent',
+                        (Qt.MatchExactly | Qt.MatchRecursive))[0],
+                1).setCurrentIndex(1)
+        # Check the 'Add scale bar' checkbox
+        self.dialog.items['Appearance'].get('Add address search').setCheckState(1, QtCore.Qt.Checked)
+        self.dialog.ol3.click()
+
+        control_file = open(
+                test_data_path(
+                        'control', 'ol3_address.html'), 'r')
+        control_output = control_file.read()
+
+
+        # Open the test file
+        test_output = read_output(self.dialog.preview.url().toString(), 'index.html')
+
+        # Compare with control file
+        self.assertEqual(test_output, control_output)
+        control_file = open(
+                test_data_path(
+                        'control', 'ol3_address.js'), 'r')
+        control_output = control_file.read()
+
+
+        # Open the test file
+        test_output = read_output(self.dialog.preview.url().toString(), 'resources/qgis2web.js')
+
+        # Compare with control file
+        self.assertEqual(test_output, control_output)
+
 def read_output(url, path):
     """ Given a url for the index.html file of a preview or export and the
     relative path to an output file open the file and return it's contents as a
