ccd219a318a6145d9498d38722ab42aad216a4fd
Tom Chadwin <tom.chadwin@nnpa.org.uk>
2016-04-20 10:26:20 +0100

    Leaflet layers list test

--- test/test_qgis2web_dialog.py
+++ test/test_qgis2web_dialog.py
@@ -1442,6 +1442,42 @@ class qgis2web_classDialogTest(unittest.TestCase):
         # Compare with control file
         self.assertEqual(test_output, control_output)
 
+    def test52_Leaflet_layerslist(self):
+        """Leaflet add layers list"""
+        layer_path = test_data_path('layer', 'airports.shp')
+        style_path = test_data_path('style', 'airports_single.qml')
+        layer = load_layer(layer_path)
+        layer.loadNamedStyle(style_path)
+
+        registry = QgsMapLayerRegistry.instance()
+        registry.addMapLayer(layer)
+
+        control_file = open(
+                test_data_path(
+                        'control', 'leaflet_layerslist.html'), 'r')
+        control_output = control_file.read()
+
+
+        # Export to web map
+        self.dialog = MainDialog(IFACE)
+        self.dialog.paramsTreeOL.itemWidget(
+                self.dialog.paramsTreeOL.findItems(
+                        'Extent',
+                        (Qt.MatchExactly | Qt.MatchRecursive))[0],
+                1).setCurrentIndex(1)
+
+        # Check the 'Add layers list' checkbox
+        self.dialog.items['Appearance'].get('Add layers list').setCheckState(1, QtCore.Qt.Checked)
+        self.dialog.leaflet.click()
+
+        # Open the test file
+        test_file = open(
+                self.dialog.preview.url().toString().replace('file://', ''))
+        test_output = test_file.read()
+
+        # Compare with control file
+        self.assertEqual(test_output, control_output)
+
 
 def read_output(url, path):
     """ Given a url for the index.html file of a preview or export and the
