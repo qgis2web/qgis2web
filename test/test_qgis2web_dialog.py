f5e9689c5ba578c9ec3778138ebb8defad272906
Tom Chadwin <tom.chadwin@nnpa.org.uk>
2016-04-20 09:29:20 +0100

    Fix Leaflet CRS test

--- test/test_qgis2web_dialog.py
+++ test/test_qgis2web_dialog.py
@@ -1354,7 +1354,9 @@ class qgis2web_classDialogTest(unittest.TestCase):
 
         registry = QgsMapLayerRegistry.instance()
         registry.addMapLayer(layer)
-
+        crs = QgsCoordinateReferenceSystem("EPSG:2964")
+        iface.mapCanvas().mapRenderer().setDestinationCrs(crs)
+        
         control_file = open(
                 test_data_path(
                         'control', 'leaflet_crs.html'), 'r')
