37a26acfc142d793aa86ec4266561bb91060836d
Tom Chadwin <tom.chadwin@nnpa.org.uk>
2016-05-25 16:57:36 +0100

    Disable 2.5d tests for 2.8

--- test/test_qgis2web_dialog.py
+++ test/test_qgis2web_dialog.py
@@ -2283,69 +2283,71 @@ class qgis2web_classDialogTest(unittest.TestCase):
 
     def test76_Leaflet_25d(self):
         """Leaflet 2.5d"""
-        layer_path = test_data_path('layer', 'lakes.shp')
-        style_path = test_data_path('style', '25d.qml')
-        layer = load_layer(layer_path)
-        layer.loadNamedStyle(style_path)
+        if os.environ["QGIS_REPO"] != "http://qgis.org/debian-ltr":
+            layer_path = test_data_path('layer', 'lakes.shp')
+            style_path = test_data_path('style', '25d.qml')
+            layer = load_layer(layer_path)
+            layer.loadNamedStyle(style_path)
 
-        registry = QgsMapLayerRegistry.instance()
-        registry.addMapLayer(layer)
+            registry = QgsMapLayerRegistry.instance()
+            registry.addMapLayer(layer)
 
-        # Export to web map
-        self.dialog = MainDialog(IFACE)
-        self.dialog.paramsTreeOL.itemWidget(
-                self.dialog.paramsTreeOL.findItems(
-                        'Extent',
-                        (Qt.MatchExactly | Qt.MatchRecursive))[0],
-                1).setCurrentIndex(1)
+            # Export to web map
+            self.dialog = MainDialog(IFACE)
+            self.dialog.paramsTreeOL.itemWidget(
+                    self.dialog.paramsTreeOL.findItems(
+                            'Extent',
+                            (Qt.MatchExactly | Qt.MatchRecursive))[0],
+                    1).setCurrentIndex(1)
 
-        self.dialog.leaflet.click()
+            self.dialog.leaflet.click()
 
-        control_file = open(
-                test_data_path(
-                        'control', 'leaflet_25d.html'), 'r')
-        control_output = control_file.read()
+            control_file = open(
+                    test_data_path(
+                            'control', 'leaflet_25d.html'), 'r')
+            control_output = control_file.read()
 
-        # Open the test file
-        test_file = open(
-                self.dialog.preview.url().toString().replace('file://', ''))
-        test_output = test_file.read()
+            # Open the test file
+            test_file = open(
+                    self.dialog.preview.url().toString().replace('file://', ''))
+            test_output = test_file.read()
 
-        # Test for expected output
-        self.assertEqual(test_output, control_output)
+            # Test for expected output
+            self.assertEqual(test_output, control_output)
 
     def test77_OL3_25d(self):
         """OL3 2.5d"""
-        layer_path = test_data_path('layer', 'lakes.shp')
-        style_path = test_data_path('style', '25d.qml')
-        layer = load_layer(layer_path)
-        layer.loadNamedStyle(style_path)
+        if os.environ["QGIS_REPO"] != "http://qgis.org/debian-ltr":
+            layer_path = test_data_path('layer', 'lakes.shp')
+            style_path = test_data_path('style', '25d.qml')
+            layer = load_layer(layer_path)
+            layer.loadNamedStyle(style_path)
 
-        registry = QgsMapLayerRegistry.instance()
-        registry.addMapLayer(layer)
+            registry = QgsMapLayerRegistry.instance()
+            registry.addMapLayer(layer)
 
-        # Export to web map
-        self.dialog = MainDialog(IFACE)
-        self.dialog.paramsTreeOL.itemWidget(
-                self.dialog.paramsTreeOL.findItems(
-                        'Extent',
-                        (Qt.MatchExactly | Qt.MatchRecursive))[0],
-                1).setCurrentIndex(1)
+            # Export to web map
+            self.dialog = MainDialog(IFACE)
+            self.dialog.paramsTreeOL.itemWidget(
+                    self.dialog.paramsTreeOL.findItems(
+                            'Extent',
+                            (Qt.MatchExactly | Qt.MatchRecursive))[0],
+                    1).setCurrentIndex(1)
 
-        self.dialog.ol3.click()
+            self.dialog.ol3.click()
 
-        control_file = open(
-                test_data_path(
-                        'control', 'ol3_25d.html'), 'r')
-        control_output = control_file.read()
+            control_file = open(
+                    test_data_path(
+                            'control', 'ol3_25d.html'), 'r')
+            control_output = control_file.read()
 
-        # Open the test file
-        test_file = open(
-                self.dialog.preview.url().toString().replace('file://', ''))
-        test_output = test_file.read()
+            # Open the test file
+            test_file = open(
+                    self.dialog.preview.url().toString().replace('file://', ''))
+            test_output = test_file.read()
 
-        # Test for expected output
-        self.assertEqual(test_output, control_output)
+            # Test for expected output
+            self.assertEqual(test_output, control_output)
 
     def test99_export_folder(self):
         """Export folder"""
