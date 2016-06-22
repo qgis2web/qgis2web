c03284a5ae70279f90c5f8684f406189adffcbed
doktoreas <luca.casagrande@gmail.com>
2016-06-22 21:38:42 +0200

    New parameter (-te_srs) for gdalwarp alg.

--- leafletLayerScripts.py
+++ leafletLayerScripts.py
@@ -204,7 +204,7 @@ def exportRasterLayer(i, safeLayerName, dataPath):
 
         processing.runalg("gdalogr:warpreproject",piped_file,
               layer.crs().authid(),"EPSG:3857","", 0, 0,
-              extentRepNew,0,4,75,6,1,False,0,False,"",piped_3857)
+              extentRepNew,"EPSG:3857",0,4,75,6,1,False,0,False,"",piped_3857)
 
 
         processing.runalg("gdalogr:translate", piped_3857, 100,
