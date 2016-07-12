c18c1b1e687a3bf04ff731204fdbd0b87f441a9a
Tom Chadwin <tom.chadwin@nnpa.org.uk>
2016-07-12 14:24:54 +0100

    OL3 scale bar control file

--- test/data/control/ol3_scalebar.js
+++ test/data/control/ol3_scalebar.js
@@ -39,7 +39,6 @@ var ALL_FIELDS = 1
  * @param layer {ol.Layer} Layer to find field info about
  */
 function getPopupFields(layerList, layer) {
-    popupLayers = [1];
     // Determine the index that the layer will have in the popupLayers Array,
     // if the layersList contains more items than popupLayers then we need to
     // adjust the index to take into account the base maps group
@@ -93,24 +92,34 @@ var onPointerMove = function(evt) {
         currentFeature = feature;
         currentLayer = layer;
         currentFeatureKeys = currentFeature.getKeys();
-        var field = getPopupFields(layersList, layer);
-        if (field == NO_POPUP) {
-        } else if (field == ALL_FIELDS) {
+        var doPopup = false;
+        for (k in layer.get('fieldImages')) {
+            if (layer.get('fieldImages')[k] != "Hidden") {
+                doPopup = true;
+            }
+        }
+        if (doPopup) {
             popupText = '<table>';
             for (var i=0; i<currentFeatureKeys.length; i++) {
                 if (currentFeatureKeys[i] != 'geometry') {
-                    popupField = '<th>' + layer.get('fieldAliases')[currentFeatureKeys[i]] + ':</th>'
-                    popupField += '<td>'+ currentFeature.get(currentFeatureKeys[i]) + '</td>';
+                    popupField = '';
+                    if (layer.get('fieldLabels')[currentFeatureKeys[i]] == "inline label") {
+                        popupField += '<th>' + layer.get('fieldAliases')[currentFeatureKeys[i]] + ':</th><td>';
+                    } else {
+                        popupField += '<td colspan="2">';
+                    }
+                    if (layer.get('fieldLabels')[currentFeatureKeys[i]] == "header label") {
+                        popupField += '<strong>' + layer.get('fieldAliases')[currentFeatureKeys[i]] + ':</strong><br />';
+                    }
+                    if (layer.get('fieldImages')[currentFeatureKeys[i]] != "Photo") {
+                        popupField += (currentFeature.get(currentFeatureKeys[i]) != null ? Autolinker.link(String(currentFeature.get(currentFeatureKeys[i]))) + '</td>' : '');
+                    } else {
+                        popupField += (currentFeature.get(currentFeatureKeys[i]) != null ? '<img src="images/' + currentFeature.get(currentFeatureKeys[i]).replace(/[\\\/:]/g, '_').trim()  + '" /></td>' : '');
+                    }
                     popupText = popupText + '<tr>' + popupField + '</tr>';
                 }
             }
             popupText = popupText + '</table>';
-        } else {
-            var value = feature.get(field);
-            if (value) {
-                popupText = '<strong>' + field + ':</strong> ' + value;
-            }  
-        }
     });
 
     if (doHighlight) {
@@ -183,9 +192,13 @@ var onSingleClick = function(evt) {
     map.forEachFeatureAtPixel(pixel, function(feature, layer) {
         currentFeature = feature;
         currentFeatureKeys = currentFeature.getKeys();
-        var field = getPopupFields(layersList, layer);
-        if (field == NO_POPUP) {
-        } else if (field == ALL_FIELDS) {
+        var doPopup = false;
+        for (k in layer.get('fieldImages')) {
+            if (layer.get('fieldImages')[k] != "Hidden") {
+                doPopup = true;
+            }
+        }
+        if (doPopup) {
             popupText = '<table>';
             for (var i=0; i<currentFeatureKeys.length; i++) {
                 if (currentFeatureKeys[i] != 'geometry') {
@@ -200,8 +213,6 @@ var onSingleClick = function(evt) {
                     }
                     if (layer.get('fieldImages')[currentFeatureKeys[i]] != "Photo") {
                         popupField += (currentFeature.get(currentFeatureKeys[i]) != null ? Autolinker.link(String(currentFeature.get(currentFeatureKeys[i]))) + '</td>' : '');
-                    } else {
-                        popupField += (currentFeature.get(currentFeatureKeys[i]) != null ? '<img src="images/' + currentFeature.get(currentFeatureKeys[i]).replace(/[\\\/:]/g, '_').trim()  + '" /></td>' : '');
                     }
                     popupText = popupText + '<tr>' + popupField + '</tr>';
                 }
