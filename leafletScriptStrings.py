441550ca262043e78298064b1517c0d675443bdc
Tom Chadwin <tom.chadwin@nnpa.org.uk>
2016-09-28 14:53:26 +0100

    Leaflet 1.0, markerCluster

--- leafletScriptStrings.py
+++ leafletScriptStrings.py
@@ -345,7 +345,7 @@ def clusterScript(safeLayerName):
     cluster = """
         var cluster_group"""
     cluster += "{safeLayerName}JSON = ".format(safeLayerName=safeLayerName)
-    cluster += """new L.MarkerClusterGroup({{showCoverageOnHover: false}});
+    cluster += """L.markerClusterGroup();
         cluster_group{safeLayerName}JSON""".format(safeLayerName=safeLayerName)
     cluster += """.addLayer(json_{safeLayerName}JSON);
 """.format(safeLayerName=safeLayerName)
