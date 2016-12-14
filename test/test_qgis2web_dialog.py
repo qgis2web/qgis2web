0344880c400d0755ab4950d36d2513d4297e2711
Tom Chadwin <tom.chadwin@nnpa.org.uk>
2016-12-14 14:58:28 +0000

    Leaflet raster test

--- test/test_qgis2web_dialog.py
+++ test/test_qgis2web_dialog.py
@@ -2494,7 +2494,7 @@ class qgis2web_classDialogTest(unittest.TestCase):
         self.assertEqual(test_output, control_output, diff(control_output, test_output))
         
         # test for exported raster file
-        assert os.path.exists(self.dialog.preview.url().toString().replace('file://', '').replace('index.html', 'data/layer_test0.png'))
+        assert os.path.exists(self.dialog.preview.url().toString().replace('file://', '').replace('index.html', 'data/test0.png'))
 
     def test79_OL3_raster(self):
         """OL3 raster"""
