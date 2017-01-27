6a9ef7d083cf891f68dc7e44e8392d79b768440f
Tom Chadwin <tom.chadwin@nnpa.org.uk>
2017-01-27 11:52:16 +0000

    Bump Proj4, Proj4Leaflet, Travis debug

--- js/proj4leaflet.js
+++ js/proj4leaflet.js
@@ -3,7 +3,7 @@
 	if (typeof define === 'function' && define.amd) {
 		// AMD
 		define(['leaflet', 'proj4'], factory);
-	} else if (typeof module !== 'undefined') {
+	} else if (typeof module === 'object' && typeof module.exports === "object") {
 		// Node/CommonJS
 		L = require('leaflet');
 		proj4 = require('proj4');
@@ -105,6 +105,7 @@
 			}
 
 			this.infinite = !this.options.bounds;
+
 		},
 
 		scale: function(zoom) {
@@ -126,9 +127,10 @@
 		},
 
 		zoom: function(scale) {
-			// Find closest number in this._scales, down 
+			// Find closest number in this._scales, down
 			var downScale = this._closestElement(this._scales, scale),
 				downZoom = this._scales.indexOf(downScale),
+				nextScale,
 				nextZoom,
 				scaleDiff;
 			// Check if scale is downScale => return array index
@@ -137,9 +139,17 @@
 			}
 			// Interpolate
 			nextZoom = downZoom + 1;
-			scaleDiff = this._scales[nextZoom] - downScale;
+			nextScale = this._scales[nextZoom];
+			if (nextScale === undefined) {
+				return Infinity;
+			}
+			scaleDiff = nextScale - downScale;
 			return (scale - downScale) / scaleDiff + downZoom;
 		},
+
+		distance: L.CRS.Earth.distance,
+
+		R: L.CRS.Earth.R,
 
 		/* Get the closest lowest element in an array */
 		_closestElement: function(array, element) {
@@ -155,20 +165,98 @@
 
 	L.Proj.GeoJSON = L.GeoJSON.extend({
 		initialize: function(geojson, options) {
-			if (geojson.crs && geojson.crs.type === 'name') {
-				var crs = new L.Proj.CRS(geojson.crs.properties.name);
-				options = options || {};
-				options.coordsToLatLng = function(coords) {
-					var point = L.point(coords[0], coords[1]);
-					return crs.projection.unproject(point);
-				};
-			}
+			this._callLevel = 0;
 			L.GeoJSON.prototype.initialize.call(this, geojson, options);
+		},
+
+		addData: function(geojson) {
+			var crs;
+
+			if (geojson) {
+				if (geojson.crs && geojson.crs.type === 'name') {
+					crs = new L.Proj.CRS(geojson.crs.properties.name);
+				} else if (geojson.crs && geojson.crs.type) {
+					crs = new L.Proj.CRS(geojson.crs.type + ':' + geojson.crs.properties.code);
+				}
+
+				if (crs !== undefined) {
+					this.options.coordsToLatLng = function(coords) {
+						var point = L.point(coords[0], coords[1]);
+						return crs.projection.unproject(point);
+					};
+				}
+			}
+
+			// Base class' addData might call us recursively, but
+			// CRS shouldn't be cleared in that case, since CRS applies
+			// to the whole GeoJSON, inluding sub-features.
+			this._callLevel++;
+			try {
+				L.GeoJSON.prototype.addData.call(this, geojson);
+			} finally {
+				this._callLevel--;
+				if (this._callLevel === 0) {
+					delete this.options.coordsToLatLng;
+				}
+			}
 		}
 	});
 
 	L.Proj.geoJson = function(geojson, options) {
 		return new L.Proj.GeoJSON(geojson, options);
+	};
+
+	L.Proj.ImageOverlay = L.ImageOverlay.extend({
+		initialize: function (url, bounds, options) {
+			L.ImageOverlay.prototype.initialize.call(this, url, null, options);
+			this._projectedBounds = bounds;
+		},
+
+		// Danger ahead: Overriding internal methods in Leaflet.
+		// Decided to do this rather than making a copy of L.ImageOverlay
+		// and doing very tiny modifications to it.
+		// Future will tell if this was wise or not.
+		_animateZoom: function (event) {
+			var scale = this._map.getZoomScale(event.zoom);
+			var northWest = L.point(this._projectedBounds.min.x, this._projectedBounds.max.y);
+			var offset = this._projectedToNewLayerPoint(northWest, event.zoom, event.center);
+
+			L.DomUtil.setTransform(this._image, offset, scale);
+		},
+
+		_reset: function () {
+			var zoom = this._map.getZoom();
+			var pixelOrigin = this._map.getPixelOrigin();
+			var bounds = L.bounds(
+				this._transform(this._projectedBounds.min, zoom)._subtract(pixelOrigin),
+				this._transform(this._projectedBounds.max, zoom)._subtract(pixelOrigin)
+			);
+			var size = bounds.getSize();
+
+			L.DomUtil.setPosition(this._image, bounds.min);
+			this._image.style.width = size.x + 'px';
+			this._image.style.height = size.y + 'px';
+		},
+
+		_projectedToNewLayerPoint: function (point, zoom, center) {
+			var viewHalf = this._map.getSize()._divideBy(2);
+			var newTopLeft = this._map.project(center, zoom)._subtract(viewHalf)._round();
+			var topLeft = newTopLeft.add(this._map._getMapPanePos());
+
+			return this._transform(point, zoom)._subtract(topLeft);
+		},
+
+		_transform: function (point, zoom) {
+			var crs = this._map.options.crs;
+			var transformation = crs.transformation;
+			var scale = crs.scale(zoom);
+
+			return transformation.transform(point, scale);
+		}
+	});
+
+	L.Proj.imageOverlay = function (url, bounds, options) {
+		return new L.Proj.ImageOverlay(url, bounds, options);
 	};
 
 	return L.Proj;
