014a24027139773e373f8e6aa944e461fb3e67df
Tom Chadwin <tom.chadwin@nnpa.org.uk>
2017-02-01 16:32:35 +0000

    Controls
    #396

--- test/data/control/ol3_precision.js
+++ test/data/control/ol3_precision.js
@@ -1,4 +1,4 @@
-var geojson_airports = {
+var geojson_airports0 = {
 "type": "FeatureCollection",
 "crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
                                                                                 
