5324e545e2f6f3b6a4bce69ce6b9297c2a2e9862
Nyall Dawson <nyall.dawson@gmail.com>
2017-02-28 16:19:40 +1000

    Remove unused saveSettings method

--- maindialog.py
+++ maindialog.py
@@ -252,24 +252,6 @@ class MainDialog(QDialog, Ui_MainDialog):
                 not os.environ.get('TRAVIS')):
             webbrowser.open_new_tab(self.exporter.destinationUrl())
 
-    def saveSettings(self, paramItem, col):
-        QgsProject.instance().removeEntry("qgis2web",
-                                          paramItem.name.replace(" ", ""))
-        if isinstance(paramItem._value, bool):
-            QgsProject.instance().writeEntry("qgis2web",
-                                             paramItem.name.replace(" ", ""),
-                                             paramItem.checkState(col))
-        else:
-            QgsProject.instance().writeEntry("qgis2web",
-                                             paramItem.name.replace(" ", ""),
-                                             paramItem.text(col))
-        if paramItem.name == "Match project CRS":
-            baseLayer = self.basemaps
-            if paramItem.checkState(col):
-                baseLayer.setDisabled(True)
-            else:
-                baseLayer.setDisabled(False)
-
     def populate_layers_and_groups(self, dlg):
         """Populate layers on QGIS into our layers and group tree view."""
         root_node = QgsProject.instance().layerTreeRoot()
