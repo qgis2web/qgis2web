597a2a6aff92e5465dd2d5722b8ab045b615e2d5
Tom Chadwin <tom.chadwin@nnpa.org.uk>
2017-05-03 16:31:26 +0100

    Fix OL heatmap test

--- test/test_qgis2web_writers.py
+++ test/test_qgis2web_writers.py
@@ -2581,15 +2581,15 @@ class qgis2web_WriterTest(unittest.TestCase):
         writer.json = [False]
 
         result = writer.write(IFACE, tempFolder()).index_file
-        test_file = open(result)
-        test_output = test_file.read()
+        # test_file = open(result)
+        # test_output = test_file.read()
 
         test_style_file = open(
             result.replace(
                 'file://', '').replace(
                     'index.html', 'layers/layers.js'))
         test_style_output = test_style_file.read()
-        test_output += test_style_output
+        test_output = test_style_output
 
         self.assertEqual(
             test_output, control_output, diff(control_output, test_output))
