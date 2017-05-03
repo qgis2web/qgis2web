90777855adb3812518a0ff514817177acc87a351
Tom Chadwin <tom.chadwin@nnpa.org.uk>
2017-05-03 16:22:47 +0100

    OL heatmap test

--- test/test_qgis2web_dialog.py
+++ test/test_qgis2web_dialog.py
@@ -2423,6 +2423,38 @@ class qgis2web_classDialogTest(unittest.TestCase):
                          [OrderedDict()])
         self.assertEqual(writer.json, [False])
 
+    def test80_OL3_heatmap(self):
+        """Dialog test: OL3 heatmap"""
+        layer_path = test_data_path('layer', 'airports.shp')
+        style_path = test_data_path('style', 'heatmap.qml')
+        layer = load_layer(layer_path)
+        layer.loadNamedStyle(style_path)
+
+        registry = QgsMapLayerRegistry.instance()
+        registry.addMapLayer(layer)
+
+        self.dialog = MainDialog(IFACE)
+        self.dialog.paramsTreeOL.itemWidget(
+            self.dialog.paramsTreeOL.findItems(
+                'Extent', (Qt.MatchExactly | Qt.MatchRecursive))[0],
+                1).setCurrentIndex(1)
+        self.setTemplate('full-screen')
+        self.dialog.ol3.click()
+
+        writer = self.dialog.createWriter()
+        self.assertTrue(isinstance(writer, OpenLayersWriter))
+        expected_params = self.defaultParams()
+        self.assertEqual(writer.params, expected_params)
+        self.assertEqual(writer.groups, {})
+        self.assertEqual(writer.layers, [layer])
+        self.assertEqual(writer.visible, [True])
+        self.assertEqual(writer.cluster, [False])
+        self.assertEqual(writer.popup,
+                         [OrderedDict([(u'ID', u'no label'), (u'fk_region', u'no label'), (u'ELEV', u'no label'), (u'NAME', u'no label'), (u'USE', u'no label')])
+                          ]
+                         )
+        self.assertEqual(writer.json, [False])
+
     def test99_export_folder(self):
         """Export folder"""
         layer_path = test_data_path('layer', 'airports.shp')
