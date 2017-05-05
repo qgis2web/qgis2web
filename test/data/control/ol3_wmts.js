2a6030d2d4904870a0a2942be1c0255a2f81b32b
Tom Chadwin <tom.chadwin@nnpa.org.uk>
2017-05-05 15:02:58 +0100

    WMTS test controls

--- test/data/control/ol3_wmts.js
+++ test/data/control/ol3_wmts.js
@@ -1,15 +1,15 @@
 
-    var projection_taiwanemapen0 = ol.proj.get('EPSG:3857');
-    var projectionExtent_taiwanemapen0 = projection_taiwanemapen0.getExtent();
-    var size_taiwanemapen0 = ol.extent.getWidth(projectionExtent_taiwanemapen0) / 256;
-    var resolutions_taiwanemapen0 = new Array(14);
-    var matrixIds_taiwanemapen0 = new Array(14);
+    var projection_wms0 = ol.proj.get('EPSG:3857');
+    var projectionExtent_wms0 = projection_wms0.getExtent();
+    var size_wms0 = ol.extent.getWidth(projectionExtent_wms0) / 256;
+    var resolutions_wms0 = new Array(14);
+    var matrixIds_wms0 = new Array(14);
     for (var z = 0; z < 14; ++z) {
         // generate resolutions and matrixIds arrays for this WMTS
-        resolutions_taiwanemapen0[z] = size_taiwanemapen0 / Math.pow(2, z);
-        matrixIds_taiwanemapen0[z] = z;
+        resolutions_wms0[z] = size_wms0 / Math.pow(2, z);
+        matrixIds_wms0[z] = z;
     }
-    var lyr_taiwanemapen0 = new ol.layer.Tile({
+    var lyr_wms0 = new ol.layer.Tile({
                             source: new ol.source.WMTS(({
                               url: "http://wmts.nlsc.gov.tw/wmts",
     attributions: [new ol.Attribution({html: '<a href=""></a>'})],
@@ -17,21 +17,21 @@
                                 "TILED": "true",
              matrixSet: 'EPSG:3857',
              format: 'image/jpeg',
-              projection: projection_taiwanemapen0,
+              projection: projection_wms0,
               tileGrid: new ol.tilegrid.WMTS({
-                origin: ol.extent.getTopLeft(projectionExtent_taiwanemapen0),
-                resolutions: resolutions_taiwanemapen0,
-                matrixIds: matrixIds_taiwanemapen0
+                origin: ol.extent.getTopLeft(projectionExtent_wms0),
+                resolutions: resolutions_wms0,
+                matrixIds: matrixIds_wms0
               }),
               style: 'default',
               wrapX: true,
                                 "VERSION": "1.0.0",
                             })),
-                            title: "taiwane-map(en)",
+                            title: "wms",
                             opacity: 1.0,
                             
                             
                           });
 
-lyr_taiwanemapen0.setVisible(true);
-var layersList = [lyr_taiwanemapen0];
+lyr_wms0.setVisible(true);
+var layersList = [lyr_wms0];
