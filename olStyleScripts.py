e35bdbf101133c8fd1cc6621ce5e3d7c72bdd507
Tom Chadwin <tom.chadwin@nnpa.org.uk>
2017-06-22 17:18:37 +0100

    OL simple pentagon marker

--- olStyleScripts.py
+++ olStyleScripts.py
@@ -338,6 +338,9 @@ def getSymbolAsStyle(symbol, stylesFolder, layer_transparency, renderer):
                 if sl.shape() == 1:
                     style = "image: %s" % getDiamond(color, borderColor,
                                                     borderWidth, size, props)
+                if sl.shape() == 2:
+                    style = "image: %s" % getPentagon(color, borderColor,
+                                                    borderWidth, size, props)
                 elif sl.shape() == 4 or sl.shape() == 5:
                     style = "image: %s" % getTriangle(color, borderColor,
                                                       borderWidth, size, props)
@@ -456,6 +459,15 @@ def getDiamond(color, borderColor, borderWidth, size, props):
             %s %s})""" % (size, stroke, getFillStyle(color, props)))
 
 
+def getPentagon(color, borderColor, borderWidth, size, props):
+    if props['outline_style'] == "no":
+        stroke = ""
+    else:
+        stroke = getStrokeStyle(borderColor, "", borderWidth, 0, 0)
+    return ("""new ol.style.RegularShape({radius: %s + size, points: 5,
+            %s %s})""" % (size, stroke, getFillStyle(color, props)))
+
+
 def getTriangle(color, borderColor, borderWidth, size, props):
     if props['outline_style'] == "no":
         stroke = ""
@@ -471,7 +483,7 @@ def getStar(color, borderColor, borderWidth, size, props):
     else:
         stroke = getStrokeStyle(borderColor, "", borderWidth, 0, 0)
     return ("""new ol.style.RegularShape({radius: %s + size, points: 5,
-            radius2: %s,%s %s})""" % (size, size/2, stroke,
+            radius2: %s, %s %s})""" % (size, size/2, stroke,
                                       getFillStyle(color, props)))
 
 
