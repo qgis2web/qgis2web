c9c3a056e478b6f3c4208fe38ad375bb31e55615
Tom Chadwin <tom.chadwin@nnpa.org.uk>
2017-09-23 15:58:11 +0100

    More default pattern/setPattern
    #275

--- olStyleScripts.py
+++ olStyleScripts.py
@@ -361,6 +361,8 @@ def getSymbolAsStyle(symbol, stylesFolder, layer_transparency, renderer, sln):
     for i in xrange(symbol.symbolLayerCount()):
         sl = symbol.symbolLayer(i)
         props = sl.properties()
+        pattern = ""
+        setPattern = ""
         if isinstance(sl, QgsSimpleMarkerSymbolLayerV2):
             color = getRGBAColor(props["color"], alpha)
             borderColor = getRGBAColor(props["outline_color"], alpha)
