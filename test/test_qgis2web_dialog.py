09d6c39e6880872ad9b3831f6ae942712d38f797
Tom Chadwin <tom.chadwin@nnpa.org.uk>
2017-10-10 12:19:01 +0100

    Test pollution

--- test/test_qgis2web_dialog.py
+++ test/test_qgis2web_dialog.py
@@ -2454,38 +2454,6 @@ class qgis2web_classDialogTest(unittest.TestCase):
                          )
         self.assertEqual(writer.json, [False])
 
-    def test81_Leaflet_heatmap(self):
-        """Dialog test: Leaflet heatmap"""
-        layer_path = test_data_path('layer', 'airports.shp')
-        style_path = test_data_path('style', 'heatmap.qml')
-        layer = load_layer(layer_path)
-        layer.loadNamedStyle(style_path)
-
-        registry = QgsMapLayerRegistry.instance()
-        registry.addMapLayer(layer)
-
-        self.dialog = MainDialog(IFACE)
-        self.dialog.paramsTreeOL.itemWidget(
-            self.dialog.paramsTreeOL.findItems(
-                'Extent', (Qt.MatchExactly | Qt.MatchRecursive))[0],
-                1).setCurrentIndex(1)
-        self.setTemplate('full-screen')
-        self.dialog.leaflet.click()
-
-        writer = self.dialog.createWriter()
-        self.assertTrue(isinstance(writer, LeafletWriter))
-        expected_params = self.defaultParams()
-        self.assertEqual(writer.params, expected_params)
-        self.assertEqual(writer.groups, {})
-        self.assertEqual(writer.layers, [layer])
-        self.assertEqual(writer.visible, [True])
-        self.assertEqual(writer.cluster, [False])
-        self.assertEqual(writer.popup,
-                         [OrderedDict([(u'ID', u'no label'), (u'fk_region', u'no label'), (u'ELEV', u'no label'), (u'NAME', u'no label'), (u'USE', u'no label')])
-                          ]
-                         )
-        self.assertEqual(writer.json, [False])
-
     def test82_OL3_WMS(self):
         """Dialog test: OL3 WMS"""
         layer_url = (
