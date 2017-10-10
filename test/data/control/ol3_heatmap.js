6fa54d260654eba073c23a7eec936735e1c5a6e4
Tom Chadwin <tom.chadwin@nnpa.org.uk>
2017-10-10 13:19:50 +0100

    OL heatmap control

--- test/data/control/ol3_heatmap.js
+++ test/data/control/ol3_heatmap.js
@@ -15,15 +15,3 @@ jsonSource_airports_0.addFeatures(features_airports_0);var lyr_airports_0 = new 
 
 lyr_airports_0.setVisible(true);
 var layersList = [lyr_airports_0];
-lyr_airports_0.set('fieldAliases', {'ID': 'ID', 'fk_region': 'fk_region', 'ELEV': 'ELEV', 'NAME': 'NAME', 'USE': 'USE', });
-lyr_airports_0.set('fieldImages', {'ID': 'TextEdit', 'fk_region': 'TextEdit', 'ELEV': 'TextEdit', 'NAME': 'TextEdit', 'USE': 'TextEdit', });
-lyr_airports_0.set('fieldLabels', {'ID': 'no label', 'fk_region': 'no label', 'ELEV': 'no label', 'NAME': 'no label', 'USE': 'no label', });
-lyr_airports_0.on('precompose', function(evt) {
-    evt.context.globalCompositeOperation = 'normal';
-});
-    lyr_airports_0.on("postcompose", update);
-
-    var listenerKey = lyr_airports_0.on('change', function(e) {
-        update();
-        ol.Observable.unByKey(listenerKey);
-    });
