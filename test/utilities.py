d0c8e60f040c9027ebf468736b8117a5452f030f
Tom Chadwin <tom.chadwin@nnpa.org.uk>
2017-10-10 16:22:12 +0100

    Test: import

--- test/utilities.py
+++ test/utilities.py
@@ -6,7 +6,7 @@ import sys
 import logging
 import urlparse
 
-from qgis.core import QgsVectorLayer, QgsRasterLayer
+from qgis.core import QgsVectorLayer, QgsRasterLayer, QgsProject
 
 
 LOGGER = logging.getLogger('QGIS')
