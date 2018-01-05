278f9cba63532558de4954b9848b029cffe1296b
Tom Chadwin <tom.chadwin@nnpa.org.uk>
2018-01-05 10:34:26 +0000

    Migrate UI FTP config dialog

--- qgis2web/ui_ftp_configuration.py
+++ qgis2web/ui_ftp_configuration.py
@@ -6,7 +6,7 @@
 #
 # WARNING! All changes made in this file will be lost!
 
-from PyQt5 import QtCore, QtGui
+from PyQt5 import QtCore, QtGui, QtWidgets
 
 try:
     _fromUtf8 = QtCore.QString.fromUtf8
@@ -15,12 +15,12 @@ except AttributeError:
         return s
 
 try:
-    _encoding = QtGui.QApplication.UnicodeUTF8
+    _encoding = QtWidgets.QApplication.UnicodeUTF8
     def _translate(context, text, disambig):
-        return QtGui.QApplication.translate(context, text, disambig, _encoding)
+        return QtWidgets.QApplication.translate(context, text, disambig, _encoding)
 except AttributeError:
     def _translate(context, text, disambig):
-        return QtGui.QApplication.translate(context, text, disambig)
+        return QtWidgets.QApplication.translate(context, text, disambig)
 
 class Ui_FtpConfiguration(object):
     def setupUi(self, FtpConfiguration):
@@ -29,42 +29,42 @@ class Ui_FtpConfiguration(object):
         icon = QtGui.QIcon()
         icon.addPixmap(QtGui.QPixmap(_fromUtf8(":/plugins/qgis2web/icons/qgis2web.png")), QtGui.QIcon.Normal, QtGui.QIcon.Off)
         FtpConfiguration.setWindowIcon(icon)
-        self.gridLayout = QtGui.QGridLayout(FtpConfiguration)
+        self.gridLayout = QtWidgets.QGridLayout(FtpConfiguration)
         self.gridLayout.setObjectName(_fromUtf8("gridLayout"))
-        self.hostLineEdit = QtGui.QLineEdit(FtpConfiguration)
+        self.hostLineEdit = QtWidgets.QLineEdit(FtpConfiguration)
         self.hostLineEdit.setObjectName(_fromUtf8("hostLineEdit"))
         self.gridLayout.addWidget(self.hostLineEdit, 0, 1, 1, 1)
-        self.usernameLineEdit = QtGui.QLineEdit(FtpConfiguration)
+        self.usernameLineEdit = QtWidgets.QLineEdit(FtpConfiguration)
         self.usernameLineEdit.setObjectName(_fromUtf8("usernameLineEdit"))
         self.gridLayout.addWidget(self.usernameLineEdit, 3, 1, 1, 1)
-        self.label_2 = QtGui.QLabel(FtpConfiguration)
+        self.label_2 = QtWidgets.QLabel(FtpConfiguration)
         self.label_2.setObjectName(_fromUtf8("label_2"))
         self.gridLayout.addWidget(self.label_2, 3, 0, 1, 1)
-        self.label = QtGui.QLabel(FtpConfiguration)
+        self.label = QtWidgets.QLabel(FtpConfiguration)
         self.label.setObjectName(_fromUtf8("label"))
         self.gridLayout.addWidget(self.label, 0, 0, 1, 1)
-        self.label_3 = QtGui.QLabel(FtpConfiguration)
+        self.label_3 = QtWidgets.QLabel(FtpConfiguration)
         self.label_3.setObjectName(_fromUtf8("label_3"))
         self.gridLayout.addWidget(self.label_3, 1, 0, 1, 1)
-        self.portSpinBox = QtGui.QSpinBox(FtpConfiguration)
+        self.portSpinBox = QtWidgets.QSpinBox(FtpConfiguration)
         self.portSpinBox.setMaximum(65535)
         self.portSpinBox.setProperty("value", 21)
         self.portSpinBox.setObjectName(_fromUtf8("portSpinBox"))
         self.gridLayout.addWidget(self.portSpinBox, 1, 1, 1, 1)
-        self.buttonBox = QtGui.QDialogButtonBox(FtpConfiguration)
-        self.buttonBox.setStandardButtons(QtGui.QDialogButtonBox.Cancel|QtGui.QDialogButtonBox.Ok)
+        self.buttonBox = QtWidgets.QDialogButtonBox(FtpConfiguration)
+        self.buttonBox.setStandardButtons(QtWidgets.QDialogButtonBox.Cancel|QtWidgets.QDialogButtonBox.Ok)
         self.buttonBox.setObjectName(_fromUtf8("buttonBox"))
         self.gridLayout.addWidget(self.buttonBox, 4, 0, 1, 2)
-        self.folderLineEdit = QtGui.QLineEdit(FtpConfiguration)
+        self.folderLineEdit = QtWidgets.QLineEdit(FtpConfiguration)
         self.folderLineEdit.setObjectName(_fromUtf8("folderLineEdit"))
         self.gridLayout.addWidget(self.folderLineEdit, 2, 1, 1, 1)
-        self.label_4 = QtGui.QLabel(FtpConfiguration)
+        self.label_4 = QtWidgets.QLabel(FtpConfiguration)
         self.label_4.setObjectName(_fromUtf8("label_4"))
         self.gridLayout.addWidget(self.label_4, 2, 0, 1, 1)
 
         self.retranslateUi(FtpConfiguration)
-        QtCore.QObject.connect(self.buttonBox, QtCore.SIGNAL(_fromUtf8("accepted()")), FtpConfiguration.accept)
-        QtCore.QObject.connect(self.buttonBox, QtCore.SIGNAL(_fromUtf8("rejected()")), FtpConfiguration.reject)
+        self.buttonBox.accepted.connect(FtpConfiguration.accept)
+        self.buttonBox.rejected.connect(FtpConfiguration.reject)
         QtCore.QMetaObject.connectSlotsByName(FtpConfiguration)
         FtpConfiguration.setTabOrder(self.hostLineEdit, self.portSpinBox)
         FtpConfiguration.setTabOrder(self.portSpinBox, self.folderLineEdit)
