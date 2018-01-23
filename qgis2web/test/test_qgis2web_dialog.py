c497ece09aee88d7f9be9907f331350cddb5e243
Tom Chadwin <tom.chadwin@nnpa.org.uk>
2018-01-23 16:45:52 +0000

    Test debug print

--- qgis2web/test/test_qgis2web_dialog.py
+++ qgis2web/test/test_qgis2web_dialog.py
@@ -179,6 +179,7 @@ class qgis2web_classDialogTest(unittest.TestCase):
         self.dialog.leaflet.click()
 
         writer = self.dialog.createWriter()
+        print(writer.params, expected_params)
         self.assertTrue(isinstance(writer, LeafletWriter))
         expected_params = self.defaultParams()
         self.assertEqual(writer.params, expected_params)
