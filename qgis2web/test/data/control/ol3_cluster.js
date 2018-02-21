6546db843991c88c4f9d3519270f9593f7b3d48d
Tom Chadwin <tom.chadwin@nnpa.org.uk>
2018-02-21 17:48:03 +0000

    Controls

--- qgis2web/test/data/control/ol3_cluster.js
+++ qgis2web/test/data/control/ol3_cluster.js
@@ -18,7 +18,7 @@ lyr_airports_0.setVisible(true);
 
 var layersList = [lyr_airports_0];
 lyr_airports_0.set('fieldAliases', {'ID': 'ID', 'fk_region': 'fk_region', 'ELEV': 'ELEV', 'NAME': 'NAME', 'USE': 'USE', });
-lyr_airports_0.set('fieldImages', {'ID': '', 'fk_region': '', 'ELEV': '', 'NAME': '', 'USE': '', });
+lyr_airports_0.set('fieldImages', {'ID': 'TextEdit', 'fk_region': 'TextEdit', 'ELEV': 'TextEdit', 'NAME': 'TextEdit', 'USE': 'TextEdit', });
 lyr_airports_0.set('fieldLabels', {'ID': 'no label', 'fk_region': 'no label', 'ELEV': 'no label', 'NAME': 'no label', 'USE': 'no label', });
 lyr_airports_0.on('precompose', function(evt) {
     evt.context.globalCompositeOperation = 'normal';
