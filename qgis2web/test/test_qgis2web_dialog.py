887cdee457e544e77be4470f6ab04f3d849008d5
Tom Chadwin <tom.chadwin@nnpa.org.uk>
2018-06-05 16:54:17 +0100

    Change test defaultParams
    #645

--- qgis2web/test/test_qgis2web_dialog.py
+++ qgis2web/test/test_qgis2web_dialog.py
@@ -90,6 +90,8 @@ class qgis2web_classDialogTest(unittest.TestCase):
                 'Measure tool': 'None',
                 'Match project CRS': False,
                 'Template': 'full-screen',
+                'Widget Background': '#ffffff',
+                'Widget Icon': '#000000',
                 'Layer search': 'None',
                 'Highlight on hover': False,
                 'Show popups on hover': False
