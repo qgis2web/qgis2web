c1fae79296c0ae8c6c71987d9f3ab62fcf8db038
Tom Chadwin <tom.chadwin@nnpa.org.uk>
2019-07-05 09:36:15 +0100

    Controls
    #770

--- qgis2web/test/data/control/ol3_address.js
+++ qgis2web/test/data/control/ol3_address.js
@@ -364,6 +364,12 @@ map.on("rendercomplete", function(evt) {
     var attributionList = attribution.getElementsByTagName('ul')[0];
     var firstLayerAttribution = attributionList.getElementsByTagName('li')[0];
     var qgis2webAttribution = document.createElement('li');
-    qgis2webAttribution.innerHTML = '<a href="https://github.com/tomchadwin/qgis2web">qgis2web</a>';
+    qgis2webAttribution.innerHTML = '<a href="https://github.com/tomchadwin/qgis2web">qgis2web</a> &middot; ';
+    var olAttribution = document.createElement('li');
+    olAttribution.innerHTML = '<a href="https://openlayers.org/">OpenLayers</a> &middot; ';
+    var qgisAttribution = document.createElement('li');
+    qgisAttribution.innerHTML = '<a href="https://qgis.org/">QGIS</a>';
     attributionList.insertBefore(qgis2webAttribution, firstLayerAttribution);
+    attributionList.insertBefore(olAttribution, firstLayerAttribution);
+    attributionList.insertBefore(qgisAttribution, firstLayerAttribution);
 })
